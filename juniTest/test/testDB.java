3da22c4b0abf4b408ad5bd33262e2e2eec215188
513283439 <513283439@qq.com>
2015-02-01 17:28:52 +0800

    1

---
+++ juniTest/test/testDB.java
@@ -0,0 +1,27 @@
+package test;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+import com.hl.databaseJDBC.JDBCConnectMySQL;
+
+
+public class testDB {
+	
+	@Test
+	public void tst() throws SQLException{
+		Connection conn = JDBCConnectMySQL.getConnection();
+		Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from userInfo");
+        while (rs.next()) {
+        	System.out.println(rs.getString("user"));
+        	System.out.println(rs.getString("name"));
+        }
+        if(conn!=null){conn.close();}
+	       
+	}
+	
+}
---
+++ src/com/hl/databaseJDBC/JDBCConnectMySQL.java
@@ -0,0 +1,20 @@
+package com.hl.databaseJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCConnectMySQL {
+	public static Connection  getConnection(){
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");     
+	        System.out.println("Success loading Mysql Driver!");
+	        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/githubliaoliaoprojectmysql","root","root");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("JDBC方式连接MySQL数据库失败");
+			conn = null;
+		}
+		return conn;
+	}
+}
