1c561deace0aa01e7202cd4059d9429295d25d37
513283439 <513283439@qq.com>
2015-02-01 18:06:47 +0800

    1

--- juniTest/test/testDB.java
+++ juniTest/test/testDB.java
@@ -3,10 +3,16 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.hl.database.JDBCConnectMySQL;
+import com.hl.database.JdbcTemplateConnectMySQL;
 
 /**
  * 测试jdbc数据库连接方式
@@ -14,6 +20,24 @@ import com.hl.database.JDBCConnectMySQL;
 public class testDB {
 	
 	@Test
+	public void jdbcTemplate(){
+		
+		JdbcTemplate jdbcTemp = JdbcTemplateConnectMySQL.getJdbcTemplate();
+		List<Map<String, Object>> lists = jdbcTemp.queryForList("select * from userInfo");
+		for(Map<String, Object> map : lists){
+			System.out.println("------");
+			System.out.println((String)map.get("user")+(String)map.get("name"));
+		}
+		  
+	}
+	
+	
+	
+	
+	
+	
+	
+	//@Test
 	public void tst() throws SQLException{
 		Connection conn = JDBCConnectMySQL.getConnection();
 		Statement stmt = conn.createStatement();
---
+++ src/com/hl/database/JdbcTemplateConnectMySQL.java
@@ -0,0 +1,19 @@
+package com.hl.database;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcTemplateConnectMySQL {
+	/**
+	 * 寻找上下文.xml文件的3个路径：
+	 * 1：类路径ClassPathXmlApplicationContext；
+	 * 2：（极少用）FileSystemXmlApplicationContext：利用文件系统中的XMl文件来载入Bean
+	 * 3: (极少用)XmlWebApplicationContext：从Web系统中的XML文件来载入Bean定义的信息
+	 * */
+	public static JdbcTemplate getJdbcTemplate(){
+		ApplicationContext  ctx = new ClassPathXmlApplicationContext("com/hl/resources/applicationContext-jdbcTemplate.xml");
+		JdbcTemplate jdbcTemp = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+		return jdbcTemp;
+	} 
+}
