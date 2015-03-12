1dcf77ac3c93efa33df68bc1898def7bea3bd022
Administrator <Administrator@WIN-KO391R46LP2>
2015-03-12 19:43:14 +0800

    -

--- WebContent/ext/ExtStudy/event.js
+++ WebContent/ext/ExtStudy/event.js
@@ -3,22 +3,23 @@ Ext.onReady(function(){
 	Ext.define('User',{
 		extend:'Ext.util.Observable',
 		constructor:function(){
-			
+			doDriver=function(){
+				this.fireEvent('driver','特论述');
+			},
 			this.addEvents(
-            	'driver'
+				{'driver':true}
         	),
-        	this.addListener('driver',function(milk){
+        	this.addListener("driver",function(milk){
         		alert(milk);
         	})        	
-		},
-		doDriver:function(){
-			this.fireEvent('driver','特论述')
 		}
-		
 	});
-	
-	var user = Ext.create('User',{});
-	user.doDriver();
+	console.log("aaaaaaacccccccccc");
+	//var useraa = Ext.create('User',{});
+	var a = new User();
+	console.log("aaaaaaa");
+	//user.doDriver();
+	//user.doDriver();
 	
 	
 });
